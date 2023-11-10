0fc507c6eb0777e8b9bb3843d00b75ef370c1e94
Ralph Landon <ralph7c2@gmail.com>
2023-11-10 09:04:59 -0700

    Update

--- main.go
+++ main.go
@@ -2,40 +2,110 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strings"
 	"time"
+
+	"github.com/spf13/cobra"
 )
 
-type Month int
-
-// const (
-//	Winter Month = iota + 1
-//	Spring
-//	Summer
-//	Autumn
-//	Fall
-// )
+var c = cobra.Command{
+	Use:   "newcal [date]",
+	Short: "Print a calendar for the given date",
+	Run:   run,
+}
 
 func main() {
+	if err := c.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(cmd *cobra.Command, args []string) {
 	t := Now()
-	fmt.Printf("Today is %s, %s %d, %d\n", t.DayOfWeek, t.Month, t.Day, t.Year)
+	nominal := "Today"
+	if len(args) > 0 {
+		var err error
+		t, err = Parse(args[0])
+		if err != nil {
+			cmd.PrintErrln(err)
+			cmd.Usage()
+			return
+		}
+		nominal = args[0]
+	}
+	if t.Day == 0 {
+		fmt.Printf("%s is LEAP DAY, %d\n", nominal, t.Year)
+		return
+	}
+	if t.Day == 37 {
+		fmt.Printf("%s is %s %d, %d\n", nominal, t.Month, t.Day, t.Year)
+		return
+	}
+	fmt.Printf("%s is %s, %s %d, %d\n", nominal, t.DayOfWeek(), t.Month, t.Day, t.Year)
+
+	printCalendar(t)
+}
+
+func printCalendar(t Time) {
+	fmt.Println("*" + strings.Repeat("-", 79) + "*")
+	fmt.Println(formatMonthStr(t.Month))
+	fmt.Printf("*-------==-------==-------==-------==-------==-------==-------==-------==-------*\n")
+	fmt.Printf("|  Mer  ||  Ven  ||  Ear  ||  Mar  ||  Jup  ||  Sat  ||  Nep  ||  Ura  ||  Plu  |\n")
+	fmt.Printf("*-------==-------==-------==-------==-------==-------==-------==-------==-------*\n")
+	start, end, weekOffset := 1, 36, 0
+	if t.Day > 37 {
+		start, end, weekOffset = 38, 73, 1
+	}
+	for i := start; i <= end; i++ {
+		if i == t.Day {
+			fmt.Printf("|  *%2d* |", i)
+		} else {
+			fmt.Printf("| %5d |", i)
+		}
+		if (i-weekOffset)%9 == 0 {
+			fmt.Printf("\n")
+		}
+	}
+	fmt.Printf("*-------==-------==-------==-------==-------==-------==-------==-------==-------*\n")
+}
+
+func formatMonthStr(month string) string {
+	monLength := len(month)
+	lineLength := 81
+	dashes := lineLength - monLength - 2
+	leftDashes := dashes / 2
+	rightDashes := dashes - leftDashes
+	return fmt.Sprintf("|%s%s%s|", strings.Repeat(" ", leftDashes), month, strings.Repeat(" ", rightDashes))
 }
 
 type Time struct {
-	Year      int
-	Month     string
-	Day       int
-	DayOfWeek string
+	Year  int
+	Month string
+	Day   int
 }
 
 func Now() Time {
 	year, month, day := FromGregorian(time.Now())
 
 	return Time{
-		Year:      year,
-		Month:     month,
-		Day:       day,
-		DayOfWeek: DayOfWeek(day),
+		Year:  year,
+		Month: month,
+		Day:   day,
 	}
+}
+
+func Parse(date string) (Time, error) {
+	t, err := time.Parse("2006-01-02", date)
+	if err != nil {
+		return Time{}, err
+	}
+	year, month, day := FromGregorian(t)
+	return Time{
+		Year:  year,
+		Month: month,
+		Day:   day,
+	}, nil
 }
 
 // Constants for seasons and month names
@@ -103,21 +173,21 @@ const (
 	Pluto     = "Pluto"
 )
 
-func DayOfWeek(newCalendarDay int) string {
-	if newCalendarDay == 37 {
+func (t Time) DayOfWeek() string {
+	if t.Day == 37 {
 		return MidSeason
 	}
-	if newCalendarDay == 0 {
+	if t.Day == 0 {
 		return "Leap Day"
 	}
-	if newCalendarDay > 37 {
-		newCalendarDay--
+	if t.Day > 37 {
+		t.Day--
 	}
 
 	// Define the days of the week in the New Calendar
 	daysOfWeek := []string{Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto}
 
 	// Calculate the day of the week
-	dayIndex := (newCalendarDay - 1) % len(daysOfWeek)
+	dayIndex := (t.Day - 1) % len(daysOfWeek)
 	return daysOfWeek[dayIndex]
 }
